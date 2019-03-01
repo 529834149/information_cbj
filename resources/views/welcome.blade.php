e5c5a00fa43f1eadf82b02fc9f444187803e3436
529834149@qq.com <529834149@qq.com>
2019-03-01 16:21:23 +0800

    no message

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel1111111111
                 </div>
 
                 <div class="links">
